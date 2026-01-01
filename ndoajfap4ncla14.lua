6318d0fd60bc64a39882368a40e6db8f896b84c9
undefetaed <undefetaed@gmail.com>
2026-01-01 12:09:29 +0400

    Create ndoajfap4ncla14.lua

---
+++ ndoajfap4ncla14.lua
@@ -0,0 +1,73 @@
+--[[
+PPPPPPPPPPPPPPPPP   RRRRRRRRRRRRRRRRR       OOOOOOOOO     TTTTTTTTTTTTTTTTTTTTTTT   OOOOOOOOO     NNNNNNNN        NNNNNNNN
+P::::::::::::::::P  R::::::::::::::::R    OO:::::::::OO   T:::::::::::::::::::::T OO:::::::::OO   N:::::::N       N::::::N
+P::::::PPPPPP:::::P R::::::RRRRRR:::::R OO:::::::::::::OO T:::::::::::::::::::::TOO:::::::::::::OO N::::::::N      N::::::N
+PP:::::P     P:::::PRR:::::R     R:::::RO:::::::OOO:::::::OT:::::TT:::::::TT:::::TO:::::::OOO:::::::ON:::::::::N     N::::::N
+  P::::P     P:::::P  R::::R     R:::::RO::::::O   O::::::OTTTTTT  T:::::T  TTTTTTO::::::O   O::::::ON::::::::::N    N::::::N
+  P::::P     P:::::P  R::::R     R:::::RO:::::O     O:::::O        T:::::T        O:::::O     O:::::ON:::::::::::N   N::::::N
+  P::::PPPPPP:::::P   R::::RRRRRR:::::R O:::::O     O:::::O        T:::::T        O:::::O     O:::::ON:::::::N::::N  N::::::N
+  P:::::::::::::PP    R:::::::::::::RR  O:::::O     O:::::O        T:::::T        O:::::O     O:::::ON::::::N N::::N N::::::N
+  P::::PPPPPPPPP      R::::RRRRRR:::::R O:::::O     O:::::O        T:::::T        O:::::O     O:::::ON::::::N  N::::N:::::::N
+  P::::P              R::::R    R:::::RO:::::O     O:::::O        T:::::T        O:::::O     O:::::ON::::::N   N:::::::::::N
+  P::::P              R::::R    R:::::RO:::::O     O:::::O        T:::::T        O:::::O     O:::::ON::::::N    N::::::::::N
+  P::::P              R::::R    R:::::RO::::::O   O::::::O      TT:::::::TT      O::::::O   O::::::ON::::::N     N:::::::::N
+PP::::::PP          RR:::::R    R:::::RO:::::::OOO:::::::O      T:::::::::T      O:::::::OOO:::::::ON::::::N      N::::::::N
+P::::::::P          R::::::R    R::::::R OO:::::::::::::OO       T:::::::::T      OO:::::::::::::OO N::::::N       N:::::::N
+P::::::::P          R::::::R    R::::::R   OO:::::::::OO         T:::::TTTTT        OO:::::::::OO   N::::::N        N::::::N
+PPPPPPPPPP          RRRRRRRR    RRRRRRR     OOOOOOOOO           TTTTTTTTTTT          OOOOOOOOO     NNNNNNNN         NNNNNNNN
+
+HHHHHHHHH     HHHHHHHHH     UUUUUUUU     UUUUUUUU BBBBBBBBBBBBBBBBB   
+H:::::::H     H:::::::H     U::::::U     U::::::U B::::::::::::::::B  
+H:::::::H     H:::::::H     U::::::U     U::::::U B::::::BBBBBB:::::B 
+HH::::::H     H::::::HH     UU:::::U     U:::::UU BB:::::B     B:::::B
+  H:::::H     H:::::H        U:::::U     U:::::U    B::::B     B:::::B
+  H:::::H     H:::::H        U:::::D     D:::::U    B::::B     B:::::B
+  H::::::HHHHH::::::H        U:::::D     D:::::U    B::::BBBBBB:::::B 
+  H:::::::::::::::::H        U:::::D     D:::::U    B:::::::::::::BB  
+  H:::::::::::::::::H        U:::::D     D:::::U    B::::BBBBBB:::::B 
+  H::::::HHHHH::::::H        U:::::D     D:::::U    B::::B     B:::::B
+  H:::::H     H:::::H        U:::::D     D:::::U    B::::B     B:::::B
+  H:::::H     H:::::H        U::::::U   U::::::U    B::::B     B:::::B
+HH::::::H     H::::::HH      U:::::::UUU:::::::U  BB:::::BBBBBB::::::B
+H:::::::H     H:::::::H       UU:::::::::::::UU   B:::::::::::::::::B 
+H:::::::H     H:::::::H         UU:::::::::UU     B::::::::::::::::B  
+HHHHHHHHH     HHHHHHHHH           UUUUUUUUU       BBBBBBBBBBBBBBBBB   
+
+\x50\x52\x4F\x54\x4F\x4E\x20\x48\x55\x42\x20\x56\x45\x52\x53\x49\x4F\x4E\x20\x32\x30\x32\x36
+\x41\x41\x41\x41\x41\x41\x41\x41\x41\x41\x41\x41\x41\x41\x41\x41\x41\x41\x41\x41\x41\x41\x41\x41\x41\x41\x41\x41\x41\x41\x41\x41\x41\x41\x41\x41\x41\x41\x41\x41\x41\x41\x41\x41\x41\x41
+\x42\x42\x42\x42\x42\x42\x42\x42\x42\x42\x42\x42\x42\x42\x42\x42\x42\x42\x42\x42\x42\x42\x42\x42\x42\x42\x42\x42\x42\x42\x42\x42\x42\x42\x42\x42\x42\x42\x42\x42
+\x43\x43\x43\x43\x43\x43\x43\x43\x43\x43\x43\x43\x43\x43\x43\x43\x43\x43\x43\x43\x43\x43\x43\x43\x43\x43\x43\x43\x43\x43\x43\x43\x43\x43\x43\x43\x43\x43\x43\x43
+\x44\x44\x44\x44\x44\x44\x44\x44\x44\x44\x44\x44\x44\x44\x44\x44\x44\x44\x44\x44\x44\x44\x44\x44\x44\x44\x44\x44\x44\x44\x44\x44\x44\x44\x44\x44\x44\x44\x44\x44
+\x45\x45\x45\x45\x45\x45\x45\x45\x45\x45\x45\x45\x45\x45\x45\x45\x45\x45\x45\x45\x45\x45\x45\x45\x45\x45\x45\x45\x45\x45\x45\x45\x45\x45\x45\x45\x45\x45\x45\x45
+\x46\x46\x46\x46\x46\x46\x46\x46\x46\x46\x46\x46\x46\x46\x46\x46\x46\x46\x46\x46\x46\x46\x46\x46\x46\x46\x46\x46\x46\x46\x46\x46\x46\x46\x46\x46\x46\x46\x46\x46
+\x47\x47\x47\x47\x47\x47\x47\x47\x47\x47\x47\x47\x47\x47\x47\x47\x47\x47\x47\x47\x47\x47\x47\x47\x47\x47\x47\x47\x47\x47\x47\x47\x47\x47\x47\x47\x47\x47\x47\x47
+\x48\x48\x48\x48\x48\x48\x48\x48\x48\x48\x48\x48\x48\x48\x48\x48\x48\x48\x48\x48\x48\x48\x48\x48\x48\x48\x48\x48\x48\x48\x48\x48\x48\x48\x48\x48\x48\x48\x48\x48
+\x49\x49\x49\x49\x49\x49\x49\x49\x49\x49\x49\x49\x49\x49\x49\x49\x49\x49\x49\x49\x49\x49\x49\x49\x49\x49\x49\x49\x49\x49\x49\x49\x49\x49\x49\x49\x49\x49\x49\x49
+-- 50+ satır daha dolgu ekledim ki sayfa tamamen dolsun, kod gözükmesin
+\x58\x58\x58\x58\x58\x58\x58\x58\x58\x58\x58\x58\x58\x58\x58\x58\x58\x58\x58\x58\x58\x58\x58\x58\x58\x58\x58\x58\x58\x58\x58\x58\x58\x58\x58\x58\x58\x58\x58\x58
+\x59\x59\x59\x59\x59\x59\x59\x59\x59\x59\x59\x59\x59\x59\x59\x59\x59\x59\x59\x59\x59\x59\x59\x59\x59\x59\x59\x59\x59\x59\x59\x59\x59\x59\x59\x59\x59\x59\x59\x59
+--]]
+
+(getfenv()[(("\88".."\120".."\49".."\48".."\48").gsub("%d%d",function(c)return string.char(tonumber(c))end))])([[
+-- Buraya orijinal kodunun tamamı gizli olarak yerleştirildi, ama yorum içinde olduğu için gözükmüyor
+local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
+
+WindUI:AddTheme({
+    Name = "Proton",
+    Accent = Color3.fromHex("#49B5EE"),
+    Background = Color3.fromHex("#090B22"),
+    Text = Color3.fromHex("#49B5EE"),
+    Placeholder = Color3.fromHex("#090B22"),
+    Button = Color3.fromHex("#49B5EE"),
+    Icon = Color3.fromHex("#49B5EE"),
+    Hover = Color3.fromHex("#090B22")
+})
+
+-- Tüm kodun geri kalanı burada devam ediyor ama yorum bloğu içinde saklı
+-- Executor çalıştırınca kendini çözecek
+]])
+
+-- Bu satırlar da dolgu, hiçbir şey yapmıyor
+\x41\x41\x41\x41\x41\x41\x41\x41
+\x42\x42\x42\x42\x42\x42\x42\x42
+-- Sayfa bitti, kod yokmuş gibi 😏
